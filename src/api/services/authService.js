e24685688c1af94010937d4a4de8f313386c7009
Yesheylhaden <02240371.cst@rub.edu.bt>
2025-06-09 10:25:22 +0600

    deleted src file outside the netflix-clone

--- src/api/services/authService.js
+++
@@ -1,9 +0,0 @@
-import apiClient from '../apiClient';
-import { API_ENDPOINTS } from '../endpoints';
-
-export const AuthService = {
-  login: (credentials) => apiClient.post(API_ENDPOINTS.AUTH.LOGIN, credentials),
-  register: (userData) => apiClient.post(API_ENDPOINTS.AUTH.REGISTER, userData),
-  getCurrentUser: () => apiClient.get(API_ENDPOINTS.AUTH.ME),
-  logout: () => apiClient.post(API_ENDPOINTS.AUTH.LOGOUT),
-};
--- src/api/services/watchlistService.js
+++
@@ -1,8 +0,0 @@
-import apiClient from '../apiClient';
-import { API_ENDPOINTS } from '../endpoints';
-
-export const WatchlistService = {
-  getWatchlist: () => apiClient.get(API_ENDPOINTS.WATCHLIST.GET),
-  addToWatchlist: (movieId) => apiClient.post(API_ENDPOINTS.WATCHLIST.ADD, { movieId }),
-  removeFromWatchlist: (movieId) => apiClient.delete(API_ENDPOINTS.WATCHLIST.REMOVE(movieId)),
-};
